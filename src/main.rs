bbb3e19d24eea1eca2cdd26f0470901e2fe02fa9
Vishal Choudhary <contactvishaltech@gmail.com>
2022-11-14 23:02:58 +0530

    impl correctness of a guess
    Signed-off-by: Vishal Choudhary <contactvishaltech@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+const GAMES: &str = include_str!("../answers.txt");
 fn main() {
-    println!("Hello, world!");
+    for answer in GAMES.split_whitespace() {
+        let guesser = wordle_solver::algorithms::Naive::new();
+        wordle_solver::play(answer, guesser);
+    }
 }
